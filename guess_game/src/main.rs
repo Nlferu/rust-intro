a12551c2ef97f51332b0a960bdb588d7fd83c6f1
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-17 23:49:11 +0100

    input library added

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Welcome in Guess Game!");
+    println!("Guess The Number!");
+
+    println!("Please input your guess: ");
+
+    let mut guess: String = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
