22b1782cd90e1cb12d377b9b8d19ce7a0ddeb096
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-19 00:03:56 +0100

    guess game improved and finished

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -2,32 +2,97 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+enum Difficulty {
+    Easy,
+    Medium,
+    Hard,
+}
+
 fn main() {
-    println!("Guess The Number!");
+    println!("Welcome In Guess The Number Game!");
+
+    println!("Please pick difficulty from following: easy, medium, hard");
+
+    let difficulty: Difficulty = loop {
+        let mut difficulty_choice: String = String::new();
+
+        io::stdin()
+            .read_line(&mut difficulty_choice)
+            .expect("Failed to read line");
+
+        let difficulty: Difficulty = match difficulty_choice.trim().to_lowercase().as_str() {
+            "easy" => Difficulty::Easy,
+            "medium" => Difficulty::Medium,
+            "hard" => Difficulty::Hard,
+            _ => {
+                println!("Please pick one of the following difficulties: easy, medium, hard");
+                continue;
+            }
+        };
+
+        match difficulty {
+            Difficulty::Easy => println!("Easy difficulty chosen"),
+            Difficulty::Medium => println!("Medium difficulty chosen"),
+            Difficulty::Hard => println!("Hard difficulty chosen"),
+        };
+
+        break difficulty;
+    };
+
+    let available_guesses: u8 = difficulty_checker(&difficulty);
+    println!("You have total of {available_guesses} available guesses");
 
     // (1..=100) `=` means that we include 100 in our scope too
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    let mut guesses_left: u8 = available_guesses;
 
-    println!("Please input your guess: ");
+    while guesses_left > 0 {
+        println!("Please input your guess: ");
 
-    // This line is creating a mutable variable that is currently bound to a new, empty instance of a String
-    let mut guess: String = String::new();
+        // This line is creating a mutable variable that is currently bound to a new, empty instance of a String
+        let mut guess: String = String::new();
 
-    io::stdin()
-        // It gives us anything that user enter as string
-        .read_line(&mut guess)
-        // It checks if input was `Ok` -> returns value or `Err` -> returns error message
-        .expect("Failed to read line");
+        io::stdin()
+            // It gives us anything that user enter as string
+            .read_line(&mut guess)
+            // It checks if input was `Ok` -> returns value or `Err` -> returns error message
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // Modified error handling
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // Instead of below:
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        guesses_left -= 1;
+        println!("You have {guesses_left} guesses left...");
+
+        if guesses_left == 0 {
+            println!("Out of guesses! The secret number was {}.", secret_number);
+        }
+    }
+}
+
+fn difficulty_checker(difficulty: &Difficulty) -> u8 {
+    match difficulty {
+        Difficulty::Easy => 20,
+        Difficulty::Medium => 10,
+        Difficulty::Hard => 5,
     }
 }
