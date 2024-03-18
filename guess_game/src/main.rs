ba2b571773a702bb1cfc5f1cf8bb7bc47604f092
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-18 22:00:06 +0100

    code explained

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -5,10 +5,13 @@ fn main() {
 
     println!("Please input your guess: ");
 
+    // This line is creating a mutable variable that is currently bound to a new, empty instance of a String
     let mut guess: String = String::new();
 
     io::stdin()
+        // It gives us anything that user enter as string
         .read_line(&mut guess)
+        // It checks if input was `Ok` -> returns value or `Err` -> returns error message
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
