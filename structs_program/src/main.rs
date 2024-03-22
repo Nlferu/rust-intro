bf914bccf9d6cd3e00fa5585e6f765bb2e650633
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-22 15:01:03 +0100

    basic and tuple solution

--- structs_program/src/main.rs
+++ structs_program/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    // Basic
+    let width: u32 = 30;
+    let height: u32 = 50;
+
+    println!(
+        "The area of recktangle is {} square pixels",
+        area(width, height)
+    );
+
+    // Tuple
+    let rectangle: (u32, u32) = (30, 50);
+
+    println!(
+        "The area_tuple of recktangle is {} square pixels",
+        area_tuple(rectangle)
+    );
+}
+
+// Basic version
+fn area(width: u32, height: u32) -> u32 {
+    return width * height;
+}
+
+// Tuples version
+fn area_tuple(dimensions: (u32, u32)) -> u32 {
+    // We dont need to use return as it is built in
+    dimensions.0 * dimensions.1
 }
