e80e21aa0d95376f7d5a6b827e2c0acada9c0563
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-22 15:12:52 +0100

    structs solution added

--- structs_program/src/main.rs
+++ structs_program/src/main.rs
@@ -1,10 +1,15 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     // Basic
     let width: u32 = 30;
     let height: u32 = 50;
 
     println!(
-        "The area of recktangle is {} square pixels",
+        "Basic: Area of recktangle is {} square pixels",
         area(width, height)
     );
 
@@ -12,8 +17,20 @@ fn main() {
     let rectangle: (u32, u32) = (30, 50);
 
     println!(
-        "The area_tuple of recktangle is {} square pixels",
+        "Tuple: Area of recktangle is {} square pixels",
         area_tuple(rectangle)
+    );
+
+    // Structs
+    let rectangle: Rectangle = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    // We are using reference here to keep `main()` as owner of this struct
+    println!(
+        "Structs: Area of recktangle is {} square pixels",
+        area_structs(&rectangle)
     );
 }
 
@@ -26,4 +43,9 @@ fn area(width: u32, height: u32) -> u32 {
 fn area_tuple(dimensions: (u32, u32)) -> u32 {
     // We dont need to use return as it is built in
     dimensions.0 * dimensions.1
+}
+
+// Structs version
+fn area_structs(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
