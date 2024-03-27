025a0ed9810e9b0f84c4d29d054d18fb590f7a21
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-27 17:50:34 +0100

    7.3 finished

--- modules/restaurant/src/lib.rs
+++ modules/restaurant/src/lib.rs
@@ -14,7 +14,40 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    // For `enum` all elements are public if we use pub before `enum`
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    // For `struct` we need to make pub each element
+    pub struct Breakfast {
+        pub toast: String,
+        _seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                _seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn _fix_incorrect_order() {
+        _cook_order();
+        super::_deliver_order();
+    }
+
+    fn _cook_order() {}
+}
+
+fn _deliver_order() {}
+
 pub fn eat_at_restaurant() {
+    // ---------------------- Path ----------------------
     // `crate` means root
 
     // Absolute path
@@ -22,4 +55,18 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // ---------------------- Struct ----------------------
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it we're not allowed to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // ---------------------- Enum ----------------------
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
 }
