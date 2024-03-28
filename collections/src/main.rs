1f7b7b1a05f7cd70c00786df670a812fdf008559
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-28 20:54:57 +0100

    updates

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -6,7 +6,38 @@ fn main() {
     // Vectors are stored on HEAP
 
     // To create a new empty vector, we call the Vec::new function
-    let v: Vec<i32> = Vec::new();
+    let _v: Vec<i32> = Vec::new();
+
+    // We can also use macro to create vector like below
+    let mut v = vec![1, 2, 3];
+
+    // ------------------------------ Updating Vector ------------------------------ \\
+    v.push(66);
 
     println!("Vector Data: {:?}", v);
+
+    // ------------------------------ Reading Vector ------------------------------ \\
+
+    // Indexing
+    let third: &i32 = &v[2];
+    println!("The third element is: {third}");
+
+    // Get
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is: {third}"),
+        None => println!("There is no third element."),
+    }
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    // This will panic without handling error
+    // let does_not_exist = &v[100];
+    // println!("Not Exist: {}", does_not_exist);
+
+    // This will let us handle error and will return None
+    let does_not_exist = v.get(100);
+    println!("Does Not Exist: {:?}", does_not_exist);
+
+    // ------------------------------ Reading Vector ------------------------------ \\
 }
