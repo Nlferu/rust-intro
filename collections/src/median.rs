3c7ca039f88d229971b3f7403586e4a19461691c
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-30 18:53:38 +0100

    median sorted vector added

--- collections/src/median.rs
+++ collections/src/median.rs
@@ -1,2 +1,10 @@
 // Given a list of integers, use a vector and return the median (when sorted, the value in the middle position)
 // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+
+pub fn median() {
+    let mut data = vec![3, -5, 8, 66, 4, -777, -33, 7, 9, 0, 88, -11];
+    data.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    // Return median & mode
+
+    println!("Sorted Data: {:?}", data);
+}
