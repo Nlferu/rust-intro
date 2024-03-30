291da619dd55ae6f6c7c37a7ecc1327d06de4cdb
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-30 21:13:27 +0100

    evaluation

--- collections/src/median.rs
+++ collections/src/median.rs
@@ -3,8 +3,10 @@
 
 pub fn median() {
     let mut even_data = vec![3, -5, 8, 66, 4, -777, -33, 7, 9, 0, 88, -11];
+    let mut odd_data = vec![3, -5, 8, 66, 4, -777, -33, 7, 9, 0, 88, -11, 99];
 
     even_data.sort();
+    odd_data.sort();
     // Return median & mode
 
     println!("Sorted Data: {:?}", even_data);
@@ -13,8 +15,15 @@ pub fn median() {
     even_median_index = even_data.len() / 2;
 
     let odd_median_index: usize;
-    odd_median_index = even_data.len() / 2 - 1;
+    odd_median_index = odd_data.len() / 2 - 1;
 
     let fin = even_data[even_median_index];
-    println!("Result: {}", fin);
+    println!("Even: {}", fin);
+
+    let calculated = even_data.len() % 2;
+
+    match calculated {
+        0 => println!("even"),
+        _ => println!("odd"),
+    }
 }
