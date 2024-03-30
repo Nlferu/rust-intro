6176e1f71cd43f142e84f8f62ed4faf93d2c2bf0
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-30 18:44:17 +0100

    converter exercise added

---
+++ collections/src/converter.rs
@@ -0,0 +1,6 @@
+// Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.”
+// Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
+
+pub fn converter() {
+    let _data_array = [3, -5, 8, 66, 4, -777, -33, 7, 9, 0, 88, -11];
+}
