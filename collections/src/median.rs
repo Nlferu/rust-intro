afc46fa43cf69994155cc5b1bd0cff33c572991c
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-03-31 01:36:15 +0100

    median calc

--- collections/src/median.rs
+++ collections/src/median.rs
@@ -1,38 +1,28 @@
 // Given a list of integers, use a vector and return the median (when sorted, the value in the middle position)
 // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
 
-pub fn median() {
+pub fn median(mut data: Vec<i32>) {
+    let median: i32;
+
     // Return median & mode
-    let _even_data = vec![3, -5, 8, 66, 4, -777, -33, 7, 9, 0, 88, -11];
-    let odd_data = vec![3, -5, 8, 66, 4, -777, -33, 7, 9, 0, 88, -11, 99];
-
-    let mut data = odd_data;
-
     data.sort();
 
     println!("Sorted Data: {:?}", data);
 
-    let even_median_index: usize;
-    even_median_index = data.len() / 2;
-
-    let odd_median_index: usize;
-    odd_median_index = data.len() / 2 - 1;
-
-    let fin = data[even_median_index];
-    println!("Even: {}", fin);
+    let median_index: usize;
+    median_index = data.len() / 2;
 
     let calculated = data.len() % 2;
-    let median: i32;
 
     match calculated {
         0 => {
             println!("Even Dataset Received");
-            median = (data[even_median_index] + data[odd_median_index]) / 2;
+            median = (data[median_index] + data[median_index - 1]) / 2;
             println!("Median Value Is: {}", median);
         }
         _ => {
             println!("Odd Dataset Received");
-            median = data[even_median_index];
+            median = data[median_index];
             println!("Median Value Is: {}", median);
         }
     }
