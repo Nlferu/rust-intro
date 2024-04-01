47cfe3b1c3b9acab8565a9b3c1bad1e5f919eeab
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-01 21:31:02 +0200

    script fixed

--- collections/src/median.rs
+++ collections/src/median.rs
@@ -2,27 +2,25 @@
 // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
 
 pub fn median(mut data: Vec<i32>) {
-    let median: i32;
+    let median: f32;
 
     // Return median & mode
     data.sort();
 
     println!("Sorted Data: {:?}", data);
 
-    let median_index: usize;
-    median_index = data.len() / 2;
-
-    let calculated = data.len() % 2;
+    let median_index: usize = data.len() / 2;
+    let calculated: usize = data.len() % 2;
 
     match calculated {
         0 => {
             println!("Even Dataset Received");
-            median = (data[median_index] + data[median_index - 1]) / 2;
+            median = (data[median_index] as f32 + data[median_index - 1] as f32) / 2.0;
             println!("Median Value Is: {}", median);
         }
         _ => {
             println!("Odd Dataset Received");
-            median = data[median_index];
+            median = data[median_index] as f32;
             println!("Median Value Is: {}", median);
         }
     }
