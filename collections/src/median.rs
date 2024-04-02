5516161b0b1df5693d3c815eaf8769443dffdf9d
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-02 15:55:25 +0200

    code shrink

--- collections/src/median.rs
+++ collections/src/median.rs
@@ -2,9 +2,6 @@
 // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
 
 pub fn median(mut data: Vec<i32>) {
-    let median: f32;
-
-    // Return median & mode
     data.sort();
 
     println!("Sorted Data: {:?}", data);
@@ -15,12 +12,12 @@ pub fn median(mut data: Vec<i32>) {
     match even_odd_checker {
         0 => {
             println!("Even Dataset Received");
-            median = (data[median_index] as f32 + data[median_index - 1] as f32) / 2.0;
+            let median: f32 = (data[median_index] as f32 + data[median_index - 1] as f32) / 2.0;
             println!("Median Value Is: {}", median);
         }
         _ => {
             println!("Odd Dataset Received");
-            median = data[median_index] as f32;
+            let median: i32 = data[median_index];
             println!("Median Value Is: {}", median);
         }
     }
