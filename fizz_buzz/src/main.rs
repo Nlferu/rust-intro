337428bd6b87532f42c6cf3cdc3d1919c5d9d0d1
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-13 20:28:27 +0200

    comments added

--- fizz_buzz/src/main.rs
+++ fizz_buzz/src/main.rs
@@ -1,4 +1,10 @@
+// Fizz buzz is a group word game. Players take turns to count incrementally.
+
+// Rules:
+// Replacing any number divisible by three with the word "fizz", and any
+// Number divisible by five with the word "buzz"
+// Number divisible by both three and five with the word "fizzbuzz"
+
 fn main() {
-    // @TODO asd
     println!("Hello, world!");
 }
