73f2a616a409baabe5b998828e0b606a03b041b9
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-16 19:38:48 +0200

    problematic assert example

--- rust_basics/src/main.rs
+++ rust_basics/src/main.rs
@@ -456,6 +456,8 @@ fn main() {
     let p = &x;
 
     println!("the memory address of x is {:p}", p); // One possible output: 0x16fa3ac84
+
+    // Problematic assert
 }
 
 // ---------------------------------------------------------------------------------- \\
