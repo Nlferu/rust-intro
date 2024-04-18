e295d716492571f5e015140d8c60703648d0571b
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-18 15:03:19 +0200

    matches macro

--- rust_basics/src/main.rs
+++ rust_basics/src/main.rs
@@ -548,6 +548,20 @@ fn main() {
 
     assert!(count == 30);
     println!("Success!");
+
+    // Matches Macro
+    let alphabets = ['a', 'E', 'Z', '0', 'x', '9', 'Y'];
+
+    // Fill the blank with `matches!` to make the code work
+    for ab in alphabets {
+        assert!(matches!(ab, 'a' | 'E' | 'Z' | '0' | 'x' | '9' | 'Y'));
+
+        // or
+
+        assert!(matches!(ab, 'a'..='z' | 'A'..='Z' | '0'..='9'))
+    }
+
+    println!("Success!");
 }
 
 // ---------------------------------------------------------------------------------- \\
