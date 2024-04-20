6c590794ac9d81c10582f684827ef7067cf28ac9
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-20 20:09:04 +0200

    updated main

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    // Rust Backtracer example
+    // backtracer();
+
+    // Proper way of handling recoverable errors
+    enum Result<T, E> {
+        Ok(T),
+        Err(E),
+    }
+}
+
+// To check which exact function caused error we can run below: RUST_BACKTRACE=1 cargo run
+fn _backtracer() {
+    a();
+}
+
+fn a() {
+    b();
+}
+
+fn b() {
+    c(22)
+}
+
+fn c(num: i32) {
+    if num == 22 {
+        // Below will handle crash of our program and print following error message
+        // Unrecoverable error
+        panic!("Don't pass in 22!")
+    }
 }
