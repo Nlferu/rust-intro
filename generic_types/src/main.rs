227ef2b9f0464e022059ade50c167aa8a224d93e
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-21 19:40:44 +0200

    struct with generic type

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,11 +1,23 @@
 use std::fmt::Debug;
 
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
 fn main() {
     let data = vec![666, 34, 3, 4, 8, 77, 98];
     let chars = vec!['y', 'a', 'z', 'c', 'x'];
 
     get_max(data);
     get_max(chars);
+
+    let p1 = Point { x: 5, y: 7 };
+    let p2 = Point { x: 5.0, y: 7.7 };
+
+    println!("x: {:?} y: {:?}", p1, p2);
 }
 
 // Universal Function With Generic Types
