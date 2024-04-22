3a42e526199c888873bfdb99d7502e9ae3a865d1
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-23 00:05:07 +0200

    traits in progress

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,16 @@
+pub struct NewsArticle {
+    pub author: String,
+    pub headline: String,
+    pub content: String,
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: String,
+    pub retweet: bool,
+}
+
 fn main() {
     println!("Hello, world!");
 }
