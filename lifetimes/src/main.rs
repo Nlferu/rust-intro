0aa4d0729f81a48588c80703bf9a7417b15d3734
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-25 21:23:12 +0200

    code to be fixed

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -4,4 +4,18 @@ fn main() {
     let r = &x;
 
     println!("r: {r}");
+
+    let string1 = String::from("absc");
+    let string2 = String::from("xyz");
+
+    let result = longest(string1.as_str(), string2.as_str());
+    println!("The longest string is: {}", result);
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
