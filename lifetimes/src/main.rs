90f128d183758f8917a30636e24718a67e05f733
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-25 23:18:16 +0200

    rules notes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -59,6 +59,13 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 // &'a i32      // a reference with an explicit lifetime
 // &'a mut i32  // a mutable reference with an explicit lifetime
 
+/////////////////
+//    RULES    //
+/////////////////
+// 1. Each parameter that is a reference gets its own lifetime parameter.
+// 2. If there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters.
+// 3. If there are multiple input lifetime parameters, but one of them is &self or &mut self the lifetime of self is assigned to all output lifetime parameters.
+
 // In this case compiler automatically added 'a lifetimes to our function below, so we do not need to specify it
 fn _first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
