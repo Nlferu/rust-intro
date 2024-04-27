1e1818ae0753f375c26ca8bc9779c68c8ebeaca4
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-27 22:21:44 +0200

    part 1 finished

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -109,5 +109,12 @@ mod tests {
         Guess::new(0);
     }
 
-    //#[test]
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("Two plus two does not equal four"))
+        }
+    }
 }
