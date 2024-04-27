21c4a65bd227e4b5467150ba2b6814cf8f9f90c9
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-27 22:19:24 +0200

    guess tests completed

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -26,8 +26,16 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100")
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}",
+                value
+            )
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}",
+                value
+            )
         }
 
         Guess { value }
@@ -93,4 +101,13 @@ mod tests {
     fn greater_than_100() {
         Guess::new(200);
     }
+
+    // Panic for only specific message
+    #[test]
+    #[should_panic(expected = "Guess value must be greater than or equal to 1")]
+    fn less_than_1() {
+        Guess::new(0);
+    }
+
+    //#[test]
 }
