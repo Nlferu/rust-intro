35dc0e6b2379697bd4ae2b0e5256a26ce5d96df2
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-27 22:02:23 +0200

    more tests examples

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -11,6 +11,10 @@ impl Rectangle {
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,7 +31,7 @@ mod tests {
             height: 1,
         };
 
-        assert!(larger.can_hold(&smaller))
+        assert!(larger.can_hold(&smaller));
     }
 
     #[test]
@@ -42,6 +46,12 @@ mod tests {
             height: 1,
         };
 
-        assert!(!smaller.can_hold(&larger))
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+        assert_ne!(4, add_two(3));
     }
 }
