c3167882c9da9a0226b9e3544c441b80bf5fd60f
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-27 21:54:21 +0200

    implementing real test

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -1,12 +1,29 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other_rectangle: &Rectangle) -> bool {
+        self.width > other_rectangle.width && self.height > other_rectangle.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*;
 
     #[test]
-    fn failint_test() {
-        panic!("Make this test fail");
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
     }
 }
