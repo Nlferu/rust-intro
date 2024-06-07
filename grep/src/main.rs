6f70630439a33124483eb32a08f947525d9b24d2
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-07 17:29:18 +0200

    reading whole file

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,9 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
     println!("Hello, grep!");
 
     let args: Vec<String> = env::args().collect();
+    // Index "0" is binary basics -> "target/debug/grep"
+    // Command: `cargo run bog poem.txt`
+    let query = &args[1]; // Something
+    let filename = &args[2]; // poem.txt
 
-    println!("{:?}", args);
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file...");
+
+    println!("Content {}", contents);
 }
