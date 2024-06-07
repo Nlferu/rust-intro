f198b82532cbf5a4f7c92e6ede1f57dbfa0efef2
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-07 19:24:07 +0200

    fn moved into impl of struct

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -22,12 +22,15 @@ struct Config {
     filename: String,
 }
 
-// We are currently cloning to avoid taking ownership of args. We can refactor this using 'lifetimes' to handle this more efficient
-fn parse_config(args: &[String]) -> Config {
-    // Index "0" is binary basics -> "target/debug/grep"
-    // Command: `cargo run bog poem.txt`
-    let query = args[1].clone(); // Something
-    let filename = args[2].clone(); // poem.txt
+impl Config {
+    // We are currently cloning to avoid taking ownership of args. We can refactor this using 'lifetimes' to handle this more efficient
+    // It is 'new' = 'constructor'
+    fn new(args: &[String]) -> Config {
+        // Index "0" is binary basics -> "target/debug/grep"
+        // Command: `cargo run bog poem.txt`
+        let query = args[1].clone(); // Something
+        let filename = args[2].clone(); // poem.txt
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
