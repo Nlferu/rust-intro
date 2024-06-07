95c6bab7c99f7794feb34ced601605ddc9ae7b92
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-07 20:23:45 +0200

    eprint mod

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        // Try running: cargo run > output.txt | cargo run to poem.txt > output.txt
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -17,7 +18,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = grep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
