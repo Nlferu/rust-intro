efe34648bba6ac70247cb8c55e426e034d236007
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-09 18:12:39 +0200

    basic iter

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3];
+
+    let v_iter = v.iter();
+
+    for value in v_iter {
+        println!("Current Value: {}", value);
+    }
 }
