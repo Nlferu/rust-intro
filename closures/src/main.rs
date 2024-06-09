3879486b31b6e0bf70a8faedb582c9306b40dfa5
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-09 16:31:19 +0200

    refactoring in progress

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -14,24 +14,57 @@ fn main() {
     generate_workout(simulated_intensity, simulated_random_number);
 }
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
+    // Example closures
+    let _example_closure_one = |x: u32| x;
+    let _example_closure = |x: u32| -> u32 { x };
+
+    // Anonymous function with 'num' parameter
+    let expensive_closure = |num| {
+        println!("Calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
-            )
+            println!("Today, run for {} minutes!", expensive_closure(intensity))
         }
     }
 }
