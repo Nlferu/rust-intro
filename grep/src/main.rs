f5ed2c4a189c3cc23c291208cd52a8f48ae08903
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-10 20:42:17 +0200

    main refactored

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -4,9 +4,7 @@ use std::process;
 use grep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         // Try running: cargo run > output.txt | cargo run to poem.txt > output.txt
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
