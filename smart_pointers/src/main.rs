2d5211b31f9c5cd8d40fdb63e4e0109c87b22412
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-13 23:20:55 +0200

    new project first chapter code

---
+++ smart_pointers/src/main.rs
@@ -0,0 +1,19 @@
+use List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
+    // Value '5' is stored on heap and 'b' is pointer for that value stored on stack
+    let b = Box::new(5);
+
+    println!("b = {}", b);
+
+    // Nil is the end of the list
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!("List: {:?}", list);
+}
