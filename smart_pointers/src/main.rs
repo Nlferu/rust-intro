4be58a50072038ce109218a5e2206ef3aa6467e6
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-14 19:46:08 +0200

    update

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,9 +1,27 @@
+use std::ops::Deref;
 use List::{Cons, Nil};
 
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    // Instead of '&T' we can do &Self::Target
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
 
 fn main() {
@@ -24,6 +42,14 @@ fn main() {
     assert_eq!(5, *y);
 
     let y = Box::new(x);
-
     assert_eq!(5, *y);
+
+    let y = MyBox::new(x);
+    // Below are the same as MyBox contain Deref trait
+    assert_eq!(5, *y);
+    assert_eq!(5, *(y.deref()));
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
