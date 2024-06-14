e36f96575050ceefbf4b5d955396aac30fe63686
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-15 01:51:57 +0200

    separated List code example

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -7,11 +7,17 @@ enum List {
     Nil,
 }
 
-pub fn main() {
+pub fn sep_main() {
     // ----------------------------------- Reference Counting -----------------------------------
 
     // We think of 'Cons' as of 'Box'
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("Count after creating a = {}", Rc::strong_count(&a));
+    let _b = Cons(3, Rc::clone(&a));
+    println!("Count after creating b = {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("Count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("Count after c goes out of scope = {}", Rc::strong_count(&a));
 }
