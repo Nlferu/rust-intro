d9fee047586c22f2975b8b33768be90b76e4ce4b
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-19 23:29:22 +0200

    code fixed

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -76,8 +76,8 @@ fn main() {
 
     // ----------------------------------- Sharing State -----------------------------------
 
-    use std::rc::Rc;
-    use std::sync::Mutex;
+    // use std::rc::Rc; -> We cannot use 'Rc' here as it is unsafe between threads, but we can fix code by using 'Arc' as below
+    use std::sync::{Arc, Mutex};
 
     let m = Mutex::new(5);
 
@@ -90,13 +90,13 @@ fn main() {
     println!("m = {:?}", m);
 
     // Counter
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
 
     // Storing all threads
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
 
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
