a6533eb300cf9240a72782683b9042de91f966d0
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-24 18:56:08 +0200

    imorting lib into main

--- trait_object/src/main.rs
+++ trait_object/src/main.rs
@@ -1,3 +1,17 @@
+use trait_object::{Button, Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // Draw SelectBox
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
