93bd6cffb8b861802fc91d827acfda3e19465df1
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-24 18:51:30 +0200

    generic approach added

--- trait_object/src/lib.rs
+++ trait_object/src/lib.rs
@@ -2,7 +2,34 @@ pub trait Draw {
     fn draw(&self);
 }
 
-pub struct Screen {
-    // 'dyn' -> stands for dynamic dispatch
-    pub components: Vec<Box<dyn Draw>>,
+// ---------------- Generic Approach ----------------
+
+// This approach is limited to store data for 1 component type, so for example slider.
+// We cannot store mixture of few components, like button, slider, background etc.
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
 }
+
+impl<T> Screen<T>
+where
+    T: Draw,
+{
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+// pub struct Screen {
+//     // 'dyn' -> stands for dynamic dispatch
+//     pub components: Vec<Box<dyn Draw>>,
+// }
+
+// impl Screen {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
