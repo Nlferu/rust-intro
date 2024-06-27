d3df1a7800db9af9c0c67b012c35718c7b9ce430
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-06-27 14:26:12 +0200

    approve fn added

--- state_object/src/lib.rs
+++ state_object/src/lib.rs
@@ -16,18 +16,27 @@ impl Post {
     }
 
     pub fn content(&self) -> &str {
+        // self.state.as_ref().unwrap().content(self)
         ""
     }
 
     pub fn request_review(&mut self) {
+        // Getting ownership of 'state' by calling 'take()' this function will also leave 'None' value in it's place after passing ownership
         if let Some(state) = self.state.take() {
             self.state = Some(state.request_review());
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.approve());
         }
     }
 }
 
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
 }
 
 struct Draft {}
@@ -36,12 +45,32 @@ impl State for Draft {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         Box::new(PendingReview {})
     }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
 }
 
 struct PendingReview {}
 
 impl State for PendingReview {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
 }
