cced08651913aa2a62e6653c972ae5e8ed59d310
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-06 21:15:49 +0200

    pattern intro

--- patterns_matching/src/main.rs
+++ patterns_matching/src/main.rs
@@ -126,4 +126,13 @@ fn main() {
     // ================================================
     //                  Pattern Syntax
     // ================================================
+
+    let x = Some(5);
+    let _y = 10;
+
+    match x {
+        Some(50) => println!("Got 50!"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
 }
