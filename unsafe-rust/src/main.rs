855e19410081b0c6743196c1a5101b73a3136766
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-21 18:43:21 +0200

    update

--- unsafe-rust/src/main.rs
+++ unsafe-rust/src/main.rs
@@ -1,3 +1,5 @@
+use core::slice;
+
 fn main() {
     let mut num = 5;
 
@@ -23,4 +25,37 @@ fn main() {
     unsafe {
         dangerous();
     }
+
+    // Safe abstraction over unsafe code
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
 }
+
+// Unsafe Implementation of below:
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+// Safe Implementation
+// It is good to use, but Rust is not smart enough to figure it out
+// fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+//     let len = slice.len();
+
+//     assert!(mid <= len);
+
+//     (&mut slice[..mid], &mut slice[mid..])
+// }
