ec37f4bff330f78a9b4f6d0644ae8fcb075d9171
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-07-21 18:33:58 +0200

    progress

--- unsafe-rust/src/main.rs
+++ unsafe-rust/src/main.rs
@@ -2,6 +2,25 @@ fn main() {
     let mut num = 5;
 
     // Raw Pointers
+    // '*' is not dereference but definition for raw pointer in below examples
     let r1 = &num as *const i32;
     let r2 = &mut num as *mut i32;
+
+    let address = 0x012345usize;
+    let r3 = address as *const i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+        println!("r3 is: {}", *r3);
+    }
+
+    unsafe fn dangerous() {}
+
+    // We cannot call it directly as usual:
+    // dangerous();
+
+    unsafe {
+        dangerous();
+    }
 }
