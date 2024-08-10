6922eeb7cae1b6cadcfc4560b6a433845a0751c5
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-08-10 15:21:27 +0200

    returning closures example

--- advanced-functions/src/main.rs
+++ advanced-functions/src/main.rs
@@ -26,4 +26,34 @@ fn main() {
     let _same_answer = do_twice_gen(add_one, 6);
 
     println!("The answer is: {}", answer);
+
+    // Function pointers
+
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+    let alt_list_of_strings: Vec<String> =
+        list_of_numbers.iter().map(ToString::to_string).collect();
+
+    println!("{:?}", list_of_strings);
+    println!("{:?}", alt_list_of_strings);
+
+    #[derive(Debug)]
+    enum Status {
+        Value(u32),
+        _Stop,
+    }
+
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+
+    println!("Statuses: {:?}", list_of_statuses);
+
+    // Returning closures
+
+    fn returns_closure() -> impl Fn(i32) -> i32 {
+        |x| x + 1
+    }
+
+    let closure = returns_closure();
+
+    println!("Closure: {}", closure(10));
 }
