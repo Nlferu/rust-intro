f877ea0ac103ed228daaaa27395c6dfbcdfe7e8f
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-08-25 18:00:29 +0200

    attribute-like and function-like macros example

--- procedural-macros/src/main.rs
+++ procedural-macros/src/main.rs
@@ -4,8 +4,6 @@ use hello_macro_derive::HelloMacro;
 #[derive(HelloMacro)]
 struct Pancakes;
 
-// Attribute-like macros
-
 fn main() {
     // Procedural macros are like functions. They take code as input, operate on that code and produce code as output.
 
@@ -15,6 +13,30 @@ fn main() {
     // * function-like
 
     Pancakes::hello_macro();
+
+    // Attribute-like macros
+
+    // #[route(GET, "/")]
+    // fn index() {
+    //     // ...
+    // }
+
+    // #[proc_macro_attribute]
+    // pub fn route(
+    //     attr: TokenStream, // GET, "/"
+    //     item: TokenStream, // fn index() {}
+    // ) -> TokenStream {
+    //     // ...
+    // }
+
+    // Function-like macros
+
+    // let sql = sql!(SELECT * FROM posts WHERE id=1);
+
+    // #[proc_macro]
+    // pub fn sql(input: TokenStream) -> TokenStream {
+    //     // ...
+    // }
 }
 
 // THIS CRATE IS USING 'hello_macro' LIB !!!
